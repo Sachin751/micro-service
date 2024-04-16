29fdc2e745135c8df022a31959a74ea65f5cce78
Sachin751 <122623762+Sachin751@users.noreply.github.com>
2024-04-15 20:50:08 -0400

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS cafe;
+USE cafe;
+
+CREATE TABLE IF NOT EXISTS cafe (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    google_maps_url VARCHAR(255) NOT NULL,
+    img_url VARCHAR(255) NOT NULL,
+    suburb VARCHAR(255) NOT NULL,
+    pricey VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255),
+    website VARCHAR(255),
+    menu VARCHAR(255)
+);
